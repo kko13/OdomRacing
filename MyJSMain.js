b1754cf4706cdf48a3550714be9f4c4f36fb19fd
Kurtis Odom <kko13@my.fsu.edu>
2018-01-18 02:24:21 -0500

    Fixing Javascript toTopButton and fading functionality

--- MyJSMain.js
+++ MyJSMain.js
@@ -1,5 +1,5 @@
 "use strict";
-var slideIndex = 1;
+var slideIndex = 0;
 
 // Scroll to top on To-Top button click
 function toTopFunction() {
@@ -9,13 +9,25 @@ function toTopFunction() {
 $(document).ready(function() {
     // Toggle fading of To-Top button if not at max height
     $(window).scroll(function() {
-    if ($(window).scrollTop() == 0) {
-        $(".tb_h1").slideDown();
-        $(".top_btn").fadeOut();
-    }
-    else {
-        $(".tb_h1").slideUp();
-        $(".top_btn").fadeIn();
-    }
+        if ($(window).scrollTop() == 0) {
+            $(".tb_h1").slideDown();
+            $(".top_btn").fadeOut();
+        }
+        else {
+            $(".tb_h1").slideUp();
+            $(".top_btn").fadeIn();
+        }
     });
 });
+
+
+function slideCount(n) {
+    slideIndex = slideIndex + n;
+    var slides = document.getElementsByClassName("slides");
+    if (slideIndex >= slides.length) {slideIndex = 0;}
+    else if(slideIndex < 0) {slideIndex = 1;}
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
