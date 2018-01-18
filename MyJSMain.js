303cae4efbb872ba71ed171868e44aaf7fdc341a
Kurtis Odom <kko13@my.fsu.edu>
2018-01-18 02:47:18 -0500

    Slight formatting change

--- MyJSMain.js
+++ MyJSMain.js
@@ -9,14 +9,14 @@ function toTopFunction() {
 $(document).ready(function() {
     // Toggle fading of To-Top button if not at max height
     $(window).scroll(function() {
-        if ($(window).scrollTop() == 0) {
-            $(".tb_h1").slideDown();
-            $(".top_btn").fadeOut();
-        }
-        else {
-            $(".tb_h1").slideUp();
-            $(".top_btn").fadeIn();
-        }
+    if ($(window).scrollTop() == 0) {
+        $(".tb_h1").slideDown();
+        $(".top_btn").fadeOut();
+    }
+    else {
+        $(".tb_h1").slideUp();
+        $(".top_btn").fadeIn();
+    }
     });
 });
 
@@ -24,8 +24,8 @@ $(document).ready(function() {
 function slideCount(n) {
     slideIndex = slideIndex + n;
     var slides = document.getElementsByClassName("slides");
-    if (slideIndex >= slides.length) {slideIndex = 0;}
-    else if(slideIndex < 0) {slideIndex = 1;}
+    if (slideIndex >= slides.length) {slideIndex = 0}
+    else if(slideIndex < 0) {slideIndex = 1}
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
